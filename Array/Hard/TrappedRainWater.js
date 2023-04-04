67592d3fe2d382a25eedc2404c34b3de18023ae0
safil badarudeen <safilbadarudeen@gmail.com>
2023-04-04 14:40:45 +0530

    added array trap rainwater efficient solution

--- Array/Hard/TrappedRainWater.js
+++ Array/Hard/TrappedRainWater.js
@@ -27,3 +27,31 @@ const getTrappedRainwater = function(heights) {
 }
 
 console.log(getTrappedRainwater(elevationArray));
+
+
+//efficient with o(n) time and O(1) space
+
+var trap = function(heights) {
+  let left = 0, right = heights.length - 1, totalWater = 0, maxLeft = 0, maxRight = 0;
+  
+  while(left < right) {
+    if(heights[left] <= heights[right]) {
+      if(heights[left] >= maxLeft) { 
+        maxLeft = heights[left]
+      } else { 
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if(heights[right] >= maxRight) {
+          maxRight = heights[right];
+      } else {
+          totalWater += maxRight - heights[right];
+      }
+        
+      right--;
+    }
+  }
+
+  return totalWater;
+};
